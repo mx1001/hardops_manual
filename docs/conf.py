cd4db94aa4e817e0ab0809557d606fdd0eb1ac5e
Jacques Lucke <mail@jlucke.com>
2016-06-04 12:01:32 +0200

    correct conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,46 +1,52 @@
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.todo'
 ]
+
+todo_include_todos = True
 
 templates_path = ['ytemplates']
 source_suffix = '.rst'
 master_doc = 'index'
 
-project = u'Code Autocomplete'
-copyright = u'2015, Jacques Lucke'
+project = u'HardOps'
+copyright = u'2015, masterxeon1001'
 
-version = '2.0.0'
-release = '2.0.0'
+version = '0.0.1'
+release = '0.0.1'
 
-exclude_patterns = []
+exclude_patterns = ['includes/*']
 
 pygments_style = 'sphinx'
 
-html_theme = 'default'
-
-html_static_path = ['ystatic']
-
-htmlhelp_basename = 'CodeAutocompleteManual'
-
+htmlhelp_basename = 'HardOpsdoc'
 
 latex_elements = {}
 latex_documents = [
-  ('index', 'CodeAutocomplete.tex', u'Code Autocomplete Manual',
-   u'Jacques Lucke', 'manual'),
+  ('index', 'HardOps.tex', u'HardOps Documentation',
+   u'masterxeon1001', 'manual'),
 ]
 
 man_pages = [
-    ('index', 'codeautocomplete', u'Code Autocomplete Manual',
-     [u'Jacques Lucke'], 1)
+    ('index', 'HardOps', u'HardOps Documentation',
+     [u'masterxeon1001'], 1)
 ]
 texinfo_documents = [
-  ('index', 'CodeAutocomplete', u'Code Autocomplete Manual',
-   u'Jacques Lucke', 'CodeAutocomplete', 'Unleash the power of scripting in Blender',
+  ('index', 'HardOps', u'HardOps Documentation',
+   u'masterxeon1001', 'HardOps', 'Modelling',
    'Miscellaneous'),
 ]
 
-epub_title = u'Code Autocomplete'
-epub_author = u'Jacques Lucke'
-epub_publisher = u'Jacques Lucke'
-epub_copyright = u'2015, Jacques Lucke'
+epub_title = u'HardOps'
+epub_author = u'masterxeon1001'
+epub_publisher = u'masterxeon1001'
+epub_copyright = u'2014, masterxeon1001'
 epub_exclude_files = ['search.html']
