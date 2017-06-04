bc5a6296bb04b2d622cf3279d829b2ba5c94dd06
niewinny <the.tecc@gmail.com>
2017-06-04 14:29:53 +0200

    Width

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 import os
 from recommonmark.parser import CommonMarkParser
 
+
+def setup(app):
+    app.add_stylesheet('my_theme.css')
+
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 source_parsers = {
